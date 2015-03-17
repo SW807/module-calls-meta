8ab88f40cbccb4022aca76ca495a4ba8be90f0a9
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-17 12:51:08 +0100

    added callhistory listener

---
+++ app/src/main/java/dk/aau/cs/psylog/module_calls_meta/CallHistoryListener.java
@@ -0,0 +1,35 @@
+package dk.aau.cs.psylog.module_calls_meta;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+import dk.aau.cs.psylog.module_lib.ISensor;
+
+public class CallHistoryListener implements SensorEventListener, ISensor {
+    @Override
+    public void startSensor() {
+        
+    }
+
+    @Override
+    public void stopSensor() {
+
+    }
+
+    @Override
+    public void sensorParameters(Intent intent) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
