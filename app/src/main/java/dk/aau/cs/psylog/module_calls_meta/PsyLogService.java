f4d65a48f64b72ac32565eb5890999b0e8ba53fb
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-17 11:25:14 +0100

    fixed gitignore in idea folder and fixed submodule name

---
+++ app/src/main/java/dk/aau/cs/psylog/module_calls_meta/PsyLogService.java
@@ -0,0 +1,10 @@
+package dk.aau.cs.psylog.module_calls_meta;
+
+import dk.aau.cs.psylog.module_lib.SensorService;
+
+public class PsyLogService extends SensorService {
+    @Override
+    public void setSensor() {
+        
+    }
+}
